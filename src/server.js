fcc81b93639bb278279fb6071225890bebd5f113
Sai Phani <ssriram@g.clemson.edu>
2022-10-30 22:37:24 -0400

    created trivia

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 // use the express library
 const express = require('express');
+const fetch = require('node-fetch');
+
 
 // create a new server application
 const app = express();
@@ -50,7 +52,45 @@ app.get('/', (req, res) => {
     visitorId:visitorId
   });
 });
+app.get("/trivia", async (req, res) => {
+  // fetch the data
+  const response = await fetch("https://opentdb.com/api.php?amount=1&type=multiple");
 
+  // fail if bad response
+  if (!response.ok) {
+    res.status(500);
+    res.send(`Open Trivia Database failed with HTTP code ${response.status}`);
+    return;
+  }
+
+  // interpret the body as json
+  const content = await response.json();
+
+  // fail if db failed
+
+  if (content.response_code !== 0) {
+    res.status(500);
+    res.send(`Open Trivia Database failed with internal response code ${content.response_code}`);
+    return;
+  }
+
+  // respond to the browser
+  // TODO: make proper html
+  answers = content.results[0].incorrect_answers 
+  random_number = Math.floor(Math.random()*answers.length)
+  const correctAnswer = content.results[0].correct_answer
+  answers.splice(random_number,0, correctAnswer )
+  
+
+const answerLinks = answers.map(answer => {
+  return `<a href="javascript:alert('${
+    answer === correctAnswer ? 'Correct!' : 'Incorrect, Please Try Again!'
+    }')">${answer}</a>
+  `
+})
+  res.render('trivia',{question:content.results[0].question,answers:answerLinks,difficulty:content.results[0].difficulty,category:content.results[0].category})
+  //res.send(JSON.stringify(content, 2));
+});
 // Start listening for network connections
 app.listen(port);
 
